a8be813dc6a4dfadd4cd476a761937d48757d9bf
davidekete <eketedavid@gmail.com>
2025-06-05 21:41:03 +0100

    experiment: migrate to vocs

---
+++ vocs.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  title: 'Solana for Developers',
+  sidebar: [
+    {
+      text: 'Getting Started',
+      link: '/getting-started',
+    },
+    {
+      text: 'The prerequisites',
+      link: '/solana-prereqs',
+    },
+  ],
+})

6ebbc1e8606716db4d4142d983a0b7bc61fba2e0
davidekete <eketedavid@gmail.com>
2025-06-09 22:35:32 +0100

    config: add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,20 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter, Geist } from "next/font/google";
+import type { ReactNode } from "react";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
+});
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist"
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={`${inter.className} ${geist.variable}`} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>

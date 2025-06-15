d45ff42cfe441bb384769a42f338a869931d246a
davidekete <eketedavid@gmail.com>
2025-06-15 14:05:30 +0100

    feat: solana prereqs, code block polishing

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -4,6 +4,7 @@ import * as TabsComponents from 'fumadocs-ui/components/tabs';
 import type { MDXComponents } from 'mdx/types';
 import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
 import * as icons from 'lucide-react';
+import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
@@ -14,5 +15,10 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     Accordion,
     Accordions,
     ...components,
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock keepBackground {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
   };
 }
